a837d21da50eec64333de17ce2c36d318d4eb5f3
Erica Bruenton <ercfit@gmail.com>
2018-07-21 11:02:14 -0600

    made changes to user and exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 
 const exerciseSchema = new mongoose.Schema({
-  name: String,
+  name: {type: String},
+  photo: {type: String},
+  video: {type: String}
 });
 
 
--- models/users.js
+++ models/users.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 const Workout = require("./workout");
-
+const Exercise = require("./exercise");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
+  username: {type: String, required: true},
+  displayName: {type: String, required: true},
+  password: {type: String, required: true},
+  email: (type: String, required: true}),
+  exercises: [Exercise.schema],
   workouts: [Workout.schema]
 });
 
