56e5cbc8291312ca587482a1aa212bd5ffbd1915
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-08-15 21:43:48 -0600

    more changes - surprise

--- db/db.js
+++ db/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/trainer';
-mongoose.connect(mongoUri);
+const Exercise = require("../models/exercise.js");
+const ExerciseData = require("../models/exerciseData.js");
 
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/project2';
+mongoose.connect(mongoUri);
 
 
 mongoose.connection.on('connected', () => {
@@ -13,10 +15,10 @@ mongoose.connection.on('error', (err) => {
   console.log(err, 'Beyonce is not ready to perform');
 });
 
-Exercise.collection.insertMany(ExerciseData, (err, data) => {
-  console.log("added exercises");
-  mongoose.connection.close();
-});
+// Exercise.collection.insertMany(ExerciseData, (err, data) => {
+//   console.log("added exercises");
+//   mongoose.connection.close();
+// });
 
 mongoose.connection.on('disconnected', () => {
   console.log('Beyonce out!');
